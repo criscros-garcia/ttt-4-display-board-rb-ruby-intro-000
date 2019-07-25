2406521f1636e9c90f738f8c16859dbffc57e8b5
Cristian Rogelio Garcia Valdez <eminemko@hotmail.com>
2019-07-25 20:00:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(array)
-  array.each do |element|
-    puts element
-  end
+def display_board(cell)
+  puts '#{cell[0]}|#{cel[1]}|#{cel[2]}'
+  puts '-----------'
+  puts '   |   |   '
+  puts '-----------'
+  puts '   |   |   '
 end
